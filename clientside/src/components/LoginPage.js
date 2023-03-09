4404ba7436bb309e1e1c8f4ba9627fe8b1ab3a7c
rapolunagarjuna <112997527+rapolunagarjuna@users.noreply.github.com>
2023-03-09 13:53:13 -0500

    Add files via upload

---
+++ clientside/src/components/LoginPage.js
@@ -0,0 +1,44 @@
+import React, { useState } from 'react';
+
+function LoginPage() {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      const response = await fetch('/api/login', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ username, password })
+      });
+      if (response.ok) {
+        window.location.href = '/dashboard';
+      } else {
+        alert('Incorrect username or password');
+      }
+    } catch (error) {
+      console.error(error);
+      alert('Error occurred while logging in');
+    }
+  };
+
+  return (
+    <div className="login-page">
+      <h1>Login</h1>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Username:
+          <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} />
+        </label>
+        <label>
+          Password:
+          <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+        </label>
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  );
+}
+
+export default LoginPage;
