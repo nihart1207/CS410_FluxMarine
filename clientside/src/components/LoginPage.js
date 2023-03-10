c77891d8436cf11aaff4260d1f01473425134e4f
nihartripathi <nihartripathi00@gmail.com>
2023-03-10 12:51:47 -0500

    Updated Login

--- clientside/src/components/LoginPage.js
+++ clientside/src/components/LoginPage.js
@@ -1,3 +1,4 @@
+import './LoginPage.css';
 import React, { useState } from 'react';
 
 function LoginPage() {
@@ -25,8 +26,10 @@ function LoginPage() {
 
   return (
     <div className="login-page">
-      <h1>Login</h1>
+      <h1>Welcome to Flux Marine</h1>
+     
       <form onSubmit={handleSubmit}>
+        <label><h2>Login</h2></label>
         <label>
           Username:
           <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} />
@@ -35,6 +38,9 @@ function LoginPage() {
           Password:
           <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
         </label>
+        <div className="forgot-password">
+        <a href="#">Forgot Password?</a>
+        </div>
         <button type="submit">Submit</button>
       </form>
     </div>
