c64f9e800b226f1bbc99a4c9e9a8fac1f3054d02
Nagarjuna Rapolu <rapolunagarjuna@gmail.com>
2023-03-26 18:29:48 -0400

    fixed the update user details bug related to  authorization

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -10,7 +10,6 @@ exports.createUser = async (req, res, next) => {
 
   
   const userExists = await User.findOne({email});
-  console.log(userExists);
   if (userExists) return res.status(409).json({message: "user already exists"});
   
 
@@ -34,32 +33,32 @@ exports.checkLogin = async (req, res, next) => {
   const {user} = req.body;
   const {username, password} = req.body;
   if (user) {
-    res.status(403).json({error: "user already loggedin"});
+    return res.status(403).json({error: "user already loggedin"});
   }
   
   try {
     if (!username || !password) {
-      res.status(400).json({error: "username/ password missing"});
+      return res.status(400).json({error: "username/ password missing"});
     } 
 
     const token = await User.matchPasswordAndGenerateToken(username, password); 
 
-    if (!token) res.status(401).json({message: "Wrong credentials"})
+    if (!token) return res.status(401).json({message: "Wrong credentials"});
     
-    res.cookie("token", token).status(200).json({message:"Log in succesful"});
+    return res.cookie("token", token).status(200).json({message:"Log in succesful"});
   } catch (err) {
-    res.status(500).json({ error: err.message });
+    return res.status(500).json({ error: err.message });
   }
 }
 
 
 //deletes a user if the user requesting is an admin and user is not deleting himself
-exports.deleteUserByIdOrEmail = async (req, res, next) => {
+exports.deleteUserByEmail = async (req, res, next) => {
   const payload = req.user;
   const {email} = req.query;
 
   if (!email) {
-    return res.status(400).json({ error: "id or email to be deleted not provided" });
+    return res.status(400).json({ error: "email to be deleted missing" });
   }
 
   if (!payload) {
@@ -91,33 +90,42 @@ exports.deleteUserByIdOrEmail = async (req, res, next) => {
 //updates user by email if and only if user requesting is self
 //only admin can change the role
 //changing token cookie too....
-exports.updateUserByIdOrEmail = async (req, res, next) => {
+exports.updateUserByEmail = async (req, res, next) => {
+
+  const payload = req.user;
+  const {email} = req.query;
+  const {password, role, name} = req.body;
+
+  if(!name || !password || !email) {
+    return res.status(400).json({ error: "email to be deleted missing" });
+  }
+
+  if (!payload) {
+    return res.status(401).json({ error: "user not authorized" });
+  }
+
+  if (role && payload.role !== "ADMIN") {
+    return res.status(401).json({error : "user not authorized"});
+  }
+
   try {
-    const {role, name, password } = req.user; 
-    const user = await User.findOne({ email: req.params.email });
+    const user = await User.findOne({email: email});
     
     if (!user) {
-      return res.status(404).json({ message: 'User not found' });
+      return res.status(404).json({ error: "User not found" });
     }
-    if (role && req.user.role === "ADMIN") user.role = role;
-    if (name && req.user.email === req.params.email) user.name = name;
-    
-    if (password && req.user.email === req.params.email) {
-      const result = await User.changePassword(email, password);
-      if (result == 0) {
-        res.status(404).json({error: "user not found"});
-      }
-    }
-    await user.save();
 
-    if (req.user.email === req.params.email) {
-      const token = createTokenForUser(user);
-      res.cookie(200).cookie('token', token).json({message : "successfully updated user"});
-    }
-    res.status(200).json({message: "successfully updated user"});
+    if (payload.email === user.email) {
+      if (name) user.name == name;
+      if (password) {
+        await User.changePassword(user.email ,password);
+      }
+
+      return res.status(200).json({message: "updated succesfully"});
+    } 
+    return res.status(401).json({error: "user not authorized"});
   } catch (err) {
-    console.error(err);
-    res.status(500).json({ error: err.message });
+    return res.status(500).json({ error: err.message });
   }
 };
 
--- server/routes/user.js
+++ server/routes/user.js
@@ -7,7 +7,7 @@ router.post('/login', userController.checkLogin);
 router.post('/signup', userController.createUser);
 router.get('/users', userController.getAllUsers);
 
-router.delete('/user/', userController.deleteUserByIdOrEmail);
-router.put('/user/', userController.updateUserByIdOrEmail);
+router.delete('/user/', userController.deleteUserByEmail);
+router.put('/user/', userController.updateUserByEmail);
 
 module.exports = router;
